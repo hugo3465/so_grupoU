d958c9ee15ce72f6c88d880e71b01a97753cfca7
Hugo Guimarães <hugo.ricardo.almeida.guimaraes@gmail.com>
2023-12-12 10:30:57 +0000

    Configuração da class CPU
    Criação da classe ConfigurationLoarder no processo

--- src/CPU.java
+++ src/CPU.java
@@ -1,6 +1,56 @@
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * unidade de processamento
  */
-public class CPU {
+public class CPU implements Runnable {
     // Gestão, escalonamento e execução de tarefas
+
+    // uma linkedBolckingQueue funciona como uma queue, mas a cabeça da fila é o
+    // elemento que está na fila há mais tempo.
+    private LinkedBlockingQueue<Runnable> taskQueue;
+
+    public CPU() {
+        this.taskQueue = new LinkedBlockingQueue<>();
+    }
+
+    // Método para adicionar uma tarefa à fila
+    public synchronized void addTask(Runnable task) {
+        try {
+            taskQueue.put(task);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    // Método para iniciar a execução da CPU
+    /*public void startExecution() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    // Obter e executar a próxima tarefa da fila
+                    Runnable task = taskQueue.take();
+                    task.run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
+    }*/
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                // Obter e executar a próxima tarefa da fila
+                Runnable task = taskQueue.take();
+                task.run();
+
+                // espera 100 milisegundos até executar a próxima tarefa da fila
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
